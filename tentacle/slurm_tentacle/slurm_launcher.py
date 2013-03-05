ad6aadd9225cdaa368a6bdc3c15c9fa3340fc613
Anders Sjogren <anders.sjogren@chalmers.se>
2013-03-05 14:10:10 +0100

    added defaults to SlurmLauncher

--- tentacle/slurm_tentacle/slurm_launcher.py
+++ tentacle/slurm_tentacle/slurm_launcher.py
@@ -1,5 +1,8 @@
+from __future__ import print_function
+
 import argparse
 from subprocess import PIPE, Popen
+import unittest
 
 from .. import utils
 
@@ -17,14 +20,14 @@ class SlurmLauncher(object):
     #        type=int, default=1, 
     #        help="The N option for sbatch. [default: %(default)s]")
         group.add_argument("--slurmPartition",
-            default="glenn",
+            default="glenn", #TODO: Remove this hardcoded default, move into config file for easy access
             help="The P option for sbatch. [default: %(default)s]")
         group.add_argument("--slurmAccount",
-            required=True,
-            help="REQUIRED: The A option for sbatch.")
+            default="SNIC001-12-175", 
+            help="The A option for sbatch. [default: %(default)s]")
         group.add_argument("--slurmJobName",
-            required=True,
-            help="REQUIRED: The J option for sbatch.")    
+            default="Tentacle",
+            help="The J option for sbatch. [default: %(default)s]")    
         group.add_argument("--slurmTimeLimit",
             default="1:00:00",
             help="The t option for sbatch. [default: %(default)s]")    
@@ -42,17 +45,18 @@ class SlurmLauncher(object):
                                "#SBATCH -t {slurmTimeLimit}"
                                ""])).format(slurmNodesPerJob=1, **options)
         jobsStr = "\n".join(commands)
+        print(setupStr + jobsStr)
         return setupStr + jobsStr
     
     def launch(self, commands, options, additional_slurm_args = []):
         script = self.create_sbatch_script(commands, options)
         call_pars = [utils.resolve_executable("sbatch")]
         call_pars.extend(additional_slurm_args)
-        print "launching: " + " ".join(call_pars) + " with input " + script
+        print("launching: " + " ".join(call_pars) + " with input " + script)
         #Make the sbatch call
         (out, err) = Popen(call_pars, stdin=PIPE, stdout=PIPE, stderr=PIPE).communicate(script)
-        print out
-        print err
+        print(out)
+        print(err)
     
     @staticmethod
     def create_python_function_command(f):
@@ -68,3 +72,8 @@ class SlurmLauncher(object):
             '. /c3se/NOBACKUP/groups/c3-snic001-12-175/activate_python', #load python module, activate virtual_env, add libs dir to LD_PATH
             self.create_python_function_command(f) ]
         self.launch(commands, options, additional_slurm_args)
+
+#class test(unittest.TestCase):
+#    def test_empty(self):
+#        sl = SlurmLauncher()
+#        sl.launch_python_function(lambda: print("yes!"),({"slurmPartition":"glenn", "slurmAccount":"001-012-175", "slurmJobName":"TentacleTest", "slurmTimeLimit":"1:00"}),({}))
